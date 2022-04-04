e3dce7941079ab9ba5e4c727bd4babb223006513
Lautaro Desouches <lautarodesouches@outlook.com>
2022-04-03 21:14:43 -0300

    show description added

--- js/index.js
+++ js/index.js
@@ -1,56 +1,57 @@
 // DOM
 
-const body      = document.getElementsByTagName('body')[0];
-const header    = document.getElementsByTagName('header')[0];
-const board     = document.getElementById('board');
-const details   = document.getElementById('details');
-const closeDet  = details.children[0];
-
-closeDet.onclick = () => {
-    details.classList.toggle('d-flex')
-}
+const body                  = document.getElementsByTagName('body')[0];
+const header                = document.getElementsByTagName('header')[0];
+const board                 = document.getElementById('board');
+const details               = document.getElementById('details');
+const closeDet              = details.children[0];
+const domCardType           = document.getElementById('cardType').children[0];
+const domCardId             = document.getElementById('cardId');
+const domCardDate           = document.getElementById('cardDate').children[0];
+const domCardTitle          = document.getElementById('cardTitle');
+const domCardDescription    = document.getElementById('cardDescription');
 
 // Variables
 
-const tipos     = [];
-let idTarjeta   = 0;
+const type = [];
+let cardID = 0;
 
 // Asignar array tarjetas del localStorage o crear array
-const tarjetas  = JSON.parse(localStorage.getItem('tarjetas')) || [];
+const cards  = JSON.parse(localStorage.getItem('cards')) || [];
 
 // Clases
 
-class Tarjeta {
+class Card {
 
-    constructor(titulo, tipo) {
-        this.titulo         = titulo;
-        this.tipo           = tipo;
-        this.id             = ++idTarjeta;
-        this.descripcion    = '';
-        this.fecha          = '';
-        this.prioridad      = '';
+    constructor(title, type) {
+        this.title          = title;
+        this.type           = type;
+        this.id             = ++cardID;
+        this.description    = '';
+        this.date           = '';
+        this.priority       = '';
     }
 
 }
 
-class Tipo {
+class Type {
 
-    constructor(nombre, id) {
-        this.nombre = nombre;
+    constructor(name, id) {
+        this.name = name;
         this.id = id;
     }
 
 }
 
 // Agregar tipos o columnas
-tipos.push(new Tipo('Lista de tareas', 'tarea'), new Tipo('En proceso', 'proceso'), new Tipo('Terminadas', 'terminada'));
+type.push(new Type('Lista de tareas', 'tarea'), new Type('En proceso', 'proceso'), new Type('Terminadas', 'terminada'));
 
 // Si hay ya hay tarjetas
-if (tarjetas.length > 0) {
+if (cards.length > 0) {
     // Almacenar ids tarjetas
-    const ids = tarjetas.map(obj => {return obj.id});
+    const ids = cards.map(obj => {return obj.id});
     // Buscar id maximo
-    idTarjeta = Math.max(...ids);
+    cardID = Math.max(...ids);
 }
 
 // Funciones
@@ -79,18 +80,17 @@ function onDrop(event) {
     // Si el lugar donde cae tiene un id que es igual a tarea, proceso o terminada
     if (dropzone.id === 'tarea' || dropzone.id === 'proceso' || dropzone.id === 'terminada') {
         // Buscar tarjeta por id
-        const resultado = tarjetas.find((el) => el.id === parseInt(id));
-        console.log(resultado)
+        const res = cards.find((el) => el.id === parseInt(id));
         // Si encuentra un resultado, cambiar tipo de tarjeta
-        resultado !== undefined && (tarjetas[tarjetas.indexOf(resultado)].tipo =  dropzone.id);
+        res !== undefined && (cards[cards.indexOf(res)].type =  dropzone.id);
     }
 
     // Recargar
-    mostrar();
+    showCards();
 
 }
 
-function mostrar() {
+function showCards() {
 
     // Limpiar
     board.innerHTML = '';
@@ -99,40 +99,41 @@ function mostrar() {
     board.innerHTML = `<div class="mt-5 row justify-content-center text-center"></div>`;
 
     // Por cada tipo crear una columna
-    tipos.forEach(tipos => {
+    type.forEach(type => {
 
         let div = document.createElement('div');
         div.classList = 'col-12 col-md-4 my-5 my-md-0';
         div.innerHTML = `
             <div class="p-3 bg-light border rounded">
-				<h5 class="pt-2">${tipos.nombre}</h5>
-				<div class="py-5 rounded" id="${tipos.id}"></div>
+				<h5 class="pt-2">${type.name}</h5>
+				<div class="py-5 rounded" id="${type.id}"></div>
 				<div class="mt-2">
 					<button class="btn btn-secondary btn-sm">+ Añadir tarjeta</button>
 				</div>
 			</div>
         `;
 
-        // Donde van a ir las tarjetas
-        const contenido = div.children[0].children[1];
+        // DOM
+        const contenido     = div.children[0].children[1];
+        const addCardBtn    = div.children[0].children[2].children[0];
 
         // Por cada tarjeta en array tarjetas
-        tarjetas.forEach(element => {
+        cards.forEach(element => {
 
             // Si el tipo de tarjeta corresponde a la columna
-            if (element.tipo === tipos.id) {
+            if (element.type === type.id) {
              
                 // Mostrar tarjeta
-                let tarjeta = document.createElement('div');
-                tarjeta.id = element.id;
-                tarjeta.classList = `row bg-white tarjeta rounded shadow-sm my-2`;
-                tarjeta.draggable = true;
-                tarjeta.innerHTML = `
+                let card = document.createElement('div');
+                card.id = element.id;
+                card.classList = `row bg-white tarjeta rounded shadow-sm my-2`;
+                card.draggable = true;
+                card.innerHTML = `
                     <div class="col-1">
                         <i class="bi bi-list"></i>
                     </div>
                     <div class="col-10 py-1">
-                        <p class="m-0">${element.titulo}</p>
+                        <p class="m-0">${element.title}</p>
                         <input type="text" class="d-none">
                     </div>
                     <div class="col-1">
@@ -140,71 +141,72 @@ function mostrar() {
                     </div>
                 `;
 
-                // Variables
-                let det = tarjeta.children[0];
-                let par = tarjeta.children[1].children[0];
-                let inp = tarjeta.children[1].children[1];
-                let del = tarjeta.children[2];
+                // DOM
+                const detailsBtn  = card.children[0];
+                const paragraph   = card.children[1].children[0];
+                const input       = card.children[1].children[1];
+                const deleteBtn   = card.children[2];
 
-                det.onclick = () => {
-                    details.classList.toggle('d-flex')
+                detailsBtn.onclick = () => {
+                    showDetails(element);
+                    details.classList.toggle('d-flex');
                 }
 
                 // Al arrastrar
-                tarjeta.ondragstart = (event) => {
+                card.ondragstart = (event) => {
                     onDragStart(event);
                 };
 
                 // Al hacer click en el parrafo
-                par.onclick = () => {
+                paragraph.onclick = () => {
                     // Ocultar parrafo
-                    par.classList.add('d-none');
+                    paragraph.classList.add('d-none');
                     // Mostrar input
-                    inp.classList.remove('d-none');
+                    input.classList.remove('d-none');
                     // Asignar texto en parrafo al valor input para modificar
-                    inp.value = par.innerText;
+                    input.value = paragraph.innerText;
                     // Hacer foco en input
-                    inp.focus();
+                    input.focus();
                 }
                 
                 // Al hacer enter
-                inp.onkeyup = (e) => {
+                input.onkeyup = (e) => {
                     if (e.keyCode === 13) {
                         // Cambiar array
-                        inp.value !== '' && (element.titulo = inp.value);
+                        input.value !== '' && (element.title = input.value);
                         // Recargar
-                        mostrar();
+                        showCards();
                     }
                 }
 
                 // Al desenfocar
-                inp.onblur = () => {
+                input.onblur = () => {
                     // Cambiar array
-                    inp.value !== '' && (element.titulo = inp.value);
+                    input.value !== '' && (element.title = input.value);
                     // Recargar
-                    mostrar();
+                    showCards();
                 }
         
                 // Al hacer click en la x roja
-                del.onclick = () => {
+                deleteBtn.onclick = () => {
                     // Buscar y borrar elemento del array
-                    tarjetas.splice(tarjetas.indexOf(element),1);
+                    cards.splice(cards.indexOf(element),1);
                     // Recargar
-                    mostrar();
+                    showCards();
                 }
 
-                contenido.appendChild(tarjeta);
-                
+                contenido.appendChild(card);
+
             }
 
         });
 
         // Al hacer click en boton agregar
-        div.children[0].children[2].children[0].onclick = () => {
+        addCardBtn.onclick = () => {
             // Agregar nueva tarjeta con el tipo correspondiente
-            tarjetas.push(new Tarjeta('Nueva tarjeta', tipos.id));
+            cards.push(new Card('Nueva tarjeta', type.id));
             // Recargar
-            mostrar();
+            showCards();
         };
 
         // Cuando un elemento se esta arrastrando por encima
@@ -219,9 +221,26 @@ function mostrar() {
     });
 
     // Actualizar tarjetas
-    localStorage.setItem('tarjetas', JSON.stringify(tarjetas));
+    localStorage.setItem('cards', JSON.stringify(cards));
 
 }
 
+function showDetails(card) {
+    domCardType.innerText = `Tipo: ${card.type}`;
+    domCardId.innerText = `ID: ${card.id}`;
+    domCardDate.innerText = (card.date !== '' ? `Fecha: ${card.date}` : 'Seleccionar fecha');
+    domCardTitle.innerText = card.title;
+    domCardDescription.innerHTML = (card.description !== '' ? card.description : 'Añadir descripcion');
+}
+
+// Eventos
+closeDet.onclick = () => {
+    details.classList.toggle('d-flex');
+}
+
+body.onkeyup = (e) => {
+    ( e.key == "Escape" && details.classList.contains('d-flex') ) && details.classList.remove('d-flex');
+}
+
 // --- Ejecutar funcion principal ---
-mostrar();
+showCards();
